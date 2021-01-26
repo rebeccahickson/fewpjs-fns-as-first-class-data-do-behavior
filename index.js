708a3752593567eb56cb034abcdd1dc87183dfea
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-26 12:42:11 -0500

    commit tree 521cd957b262286c65cf0a0053d8096e6d681bd4

--- index.js
+++ index.js
@@ -3,11 +3,23 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(string) {
+  const time = parseInt(string.split(":"));
+  if (time < 12) {
+    return "Good Morning";
+  } else if (time > 17) {
+    return "Good Evening";
+  } else {
+    return "Good Afternoon";
+  }
+}
+
+function displayMessage(string) {
+  document.querySelector("h1#greeting").innerText = string;
+}
